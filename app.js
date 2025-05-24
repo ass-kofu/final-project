93126eea4d396c54344dae7be52e6e1da57c6b87
ass-kofu <132200115+ass-kofu@users.noreply.github.com>
2025-05-24 09:18:35 +0300

    final

--- app.js
+++ app.js
@@ -57,8 +57,75 @@ const addToCart = (product_id) => {
   }else{
     carts[positionThisProductInCart].quantity = carts[positionThisProductInCart].quantity = 1;
   }
+  addCartToHTML();
+  addCartToMemory();
   }
-    console.log('carts');
+  const addCartToMemory = () => {
+    localStorage.setItem('cart', JSON.stringify(carts));
+  }
+  const addCartToHTML = () => {
+    listCartHTML.innerHTML = "";
+    let totalQuantity = 0;
+    if(carts.length > 0){
+      carts.forEach(cart => {
+        totalQuantity = totalQuantity + cart.quantity;
+        let newCart = document.createElement('div');
+        newCart.classList.add('item');
+        newCart.dataset.id = cart.product_id;
+        let positionProduct = listProducts.findIndex((value) => value.id == cart.product_id);
+        let info = listProducts[positionProduct];
+        newCart.innerHTML = `
+        <div class="image">
+          <img src="${info.image}" alt="">
+        </div>
+        <div class="name">
+          ${info.name}
+        </div>
+        <div class="totalPrice">
+          Ksh ${info.price * cart.quantity}
+        </div>
+        <div class="quantity">
+          <span class="minus">-</span>
+          <span>${cart.quantity}</span>
+          <span class="plus">+</span>
+        </div>`;
+        listCartHTML.appendChild(newCart);
+      })
+    }
+    iconCartSpan.innerText = totalQuantity;
+  }
+  listCartHTML.addEventListener('click',(event) => {
+    let positionClick = event.target;
+    if(positionClick.classList.contains('minus') || positionClick.classList.contains('plus')){
+    let product_id = positionClick.parentElement.parentElement.dataset.id;
+       let type = 'minus';
+       if(positionClick.classList.contains('plus')){
+        type = 'plus';
+       }
+       changeQuantity(product_id, type);
+    }
+  })
+  const changeQuantity = (product_id, type) => {
+    let positionItemInCart = carts.findIndex((value) => value.product_id == product_id);
+    if(positionItemInCart >= 0){
+      switch (type) {
+        case 'plus':
+        carts[positionItemInCart].quantity = carts[positionItemInCart].quantity + 1;
+        break;
+
+        default:
+          let valueChange = carts[positionItemInCart].quantity - 1;
+          if(valueChange > 0){
+            carts[positionItemInCart].quantity = valueChange;
+          }else{
+            carts.splice(positionItemInCart, 1);
+          }
+          break;
+      }
+    }
+    addCartToMemory();
+    addCartToHTML();
+  }
 
 const initAPP = () => {
   //GET DATA FROM JSON
@@ -67,6 +134,12 @@ const initAPP = () => {
   .then(data => {
     listProducts = data;
     addDataToHTML();
+
+    //add cart to memory
+    if(localStorage.getItem('cart')){
+      carts = JSON.parse(localStorage.getItem('cart'));
+      addCartToHTML();
+    }
   })
 }
 initAPP();
